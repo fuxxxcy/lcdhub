e0f718ec79b313e301e30cb596a6f1b301481e55
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-07-07 18:56:12 +0300

    Add path aliases, cosmetic changes

---
+++ client/config-overrides.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = function override(config) {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, 'src'),
+    '@components': path.resolve(__dirname, 'src/components')
+  };
+
+  return config;
+};
