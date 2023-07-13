33e5c066cc00a6c9efd8faae0cfca2d9c6ee797c
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-07-13 19:24:05 +0300

    bug fix, add tailwindcss

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from '@/AppRoutes';
 import { RouterProvider } from 'react-router-dom';
+import "@/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- client/src/pages/ProjectsPage.tsx
+++ client/src/pages/ProjectsPage.tsx
@@ -14,16 +14,19 @@ export default function ProjectsPage() {
   }, [LoaderData]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center">
       {
         LoaderData === undefined ? <span>loading...</span> : 
         projects === undefined ? <span>no data here</span> : 
         projects.map((project, idx) => {
-          return <div key={idx}>
-            <div>{project.name}</div>
-            <img src={project.image} alt="" width="200px" />
-            <div>{project.description}</div>
-          </div>
+          return <a key={idx} href={`project/${project.id}`} className="flex flex-row rounded-3xl w-3/5 p-2.5 m-5 border-2 border-black gap-x-2.5">
+            <img src={project.image} alt="" className="aspect-square w-2/12 rounded-2xl" />
+            <div>
+              <span className="text-2xl font-bold">{project.name}</span>
+              <br />
+              <span>{project.description.slice(0, 100) + "..."}</span>
+            </div>
+          </a>
         })
       }
     </div>
