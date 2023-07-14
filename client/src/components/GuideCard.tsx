f5fd515a1c082f920ccda18ef59a2c20fbd8a0ed
fuxxxcy <andrey121775@gmail.com>
2023-07-14 15:52:11 +0300

    client pages upd

--- client/src/components/GuideCard.tsx
+++ client/src/components/GuideCard.tsx
@@ -1,10 +1,29 @@
+import TimeFrame from "@/images/TimeFrame";
+
 const GuideCard = () => {
   return (
     <div className='guide_card'>
-      <div className="guide_card__logo">place for logo</div>
-      <div className="guide_card__button">
-        <div className="guide_card__button__text">Перейти</div>
+
+      <div className="guide_card__firstblock">
+        <div className="guide_card__logo">place for logo</div>
+        <div className="guide_card__button">
+          <div className="guide_card__button__text">Перейти</div>
+        </div>
       </div>
+
+      <div className="guide_card_secondblock">
+        <div className="guide_card__info">
+          <div className="guide_card__info__time">
+            <div className="guide_card__info__time__logo"><TimeFrame/></div>
+            <div className="guide_card__info__time__text">2 мин</div>
+          </div>
+          <div className="guide_card__info__price">
+            <div className="guide_card__info__price__logo"></div>
+            <div className="guide_card__info__price__text">Бесплатно</div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
