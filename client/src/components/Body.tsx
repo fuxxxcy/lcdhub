533c6ba914835547c815974f2a67fa9e9c020d8e
fuxxxcy <andrey121775@gmail.com>
2023-07-14 12:36:42 +0300

    client, creating main page

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import "./styles.css";
 import Stroke from '@/images/Stroke';
+import GuideCard from './GuideCard';
 
 const Body = () => {
     return (
         <div className='body'>
-            <div className='body_filter'>
-                <div className='body_filter__default body_filter__all'>Все</div>
-                <div className='body_filter_stroke'><Stroke/></div>
-                <div className='body_filter__default body_filter__testnet'>Тестнеты</div>
-                <div className='body_filter_stroke'><Stroke/></div>
-                <div className='body_filter__default body_filter__retrodrop'>Ретродропы</div>
+            <div className='body_filters'>
+                <div className='body_filter'>
+                    <div className='body_filter__default body_filter__all'>Все</div>
+                    <div className='body_filter_stroke'><Stroke/></div>
+                    <div className='body_filter__default body_filter__testnet'>Тестнеты</div>
+                    <div className='body_filter_stroke'><Stroke/></div>
+                    <div className='body_filter__default body_filter__retrodrop'>Ретродропы</div>
+                </div>
+                <div className="body_search">
+                    <div className="body_search__text">Поиск</div>
+                </div>
             </div>
-            <div className="body_search"></div>
+
+            <div className="guide_cards">
+                <GuideCard/>
+                <GuideCard/>
+                <GuideCard/>
+            </div>
         </div>
     )
 }
